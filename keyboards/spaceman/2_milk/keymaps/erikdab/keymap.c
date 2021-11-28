445ae20095047a3cacdb2abf3dbc9336a1f10514
Erik David Burnell <erik.d.burnell@gmail.com>
2021-11-28 15:59:25 -0600

    Nano Trackball added tap dance for caps lock key

--- keyboards/spaceman/2_milk/keymaps/erikdab/keymap.c
+++ keyboards/spaceman/2_milk/keymaps/erikdab/keymap.c
@@ -1,8 +1,26 @@
 #include QMK_KEYBOARD_H
 
+enum tapdance_keycodes {
+    TD_KEY_2,
+};
+
+void dance_key_two (qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        tap_code(KC_BTN2);
+        reset_tap_dance (state);
+    } else if (state->count == 2) {
+        tap_code(KC_CAPS);
+        reset_tap_dance (state);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_KEY_2] = ACTION_TAP_DANCE_FN(dance_key_two),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [0] = LAYOUT(
           KC_BTN1,
-          KC_BTN2	
+          TD(TD_KEY_2)
           )
 };
---
+++ keyboards/spaceman/2_milk/keymaps/erikdab/readme.md
@@ -0,0 +1,3 @@
+# The erikdab keymap for PloopyCo Mouse
+
+It is used to trigger left and right mouse buttons as well as toggling caps lock state which toggles scroll mode on the Trackball Nano.
