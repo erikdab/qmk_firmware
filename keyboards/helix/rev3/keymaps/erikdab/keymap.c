1fe11ec5e28d8fd6b1d6691f40d89563a8eaf7f6
Erik Burnell <erik.d.burnell@gmail.com>
2026-04-01 17:56:17 -0700

    next

--- keyboards/helix/rev3/keymaps/erikdab/keymap.c
+++ keyboards/helix/rev3/keymaps/erikdab/keymap.c
@@ -15,124 +15,85 @@
  */
 #include QMK_KEYBOARD_H
 
-
-// Defines names for use in layer keycodes and the keymap
+// Layer names
+// _GAME  : primary left-hand-only gaming layer
+// _SWAP   : extra numbers and punctuation used in some games
+// _FUNC   : arrows, F1-F12, media, and navigation shortcuts
+// _UTIL  : GUI, delete/backspace, boot/reset, and admin keys
 enum layer_names {
-  _QWERTY = 0,
-  _SWAP,
-  _FUNCTION,
-  _SWAPFX
+    _GAME = 0,
+    _UTIL,
+    _FUNC,
+    _SWAP,
 };
 
-// Defines the keycodes used by our macros in process_record_user
+// Custom keycodes
+// ADJUST toggles the admin/media layer on and off.
 enum custom_keycodes {
-  SWAPFX = SAFE_RANGE
+    ADJUST = SAFE_RANGE,
 };
 
-#define SWAP MO(_SWAP)
-#define FUNCTION MO(_FUNCTION)
+// Thumb and layer helpers
+#define UTIL     MO(_UTIL)
+#define FUNC      MO(_FUNC)
+#define SWAP     MO(_SWAP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Qwerty
-   * ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |
-   * |------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |
-   * |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |
-   * |------+------+------+------+------+------+------.
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | Enter|
-   * |------+------+------+------+------+------+------|
-   * |SwapFX| Esc  | Alt  | GUI  | Func | SWAP | Space|
-   * `------------------------------------------------'
-   */
-  [_QWERTY] = LAYOUT_5row( \
-      KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,       KC_5,                      _______, _______, _______, _______, _______, _______, \
-      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,                      _______, _______, _______, _______, _______, _______, \
-      KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,       KC_G,                      _______, _______, _______, _______, _______, _______, \
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_DEL, _______,  _______, _______, _______, _______, _______, _______, \
-      SWAPFX,    KC_ESC,  KC_LALT, KC_SPC , FUNCTION,   SWAP,    KC_ENT, _______,  _______, _______, _______, _______, _______, _______ \
+    /* GAME
+     * Left-hand-only gaming layer for a single Helix half.
+     *
+     * Design goals:
+     * - direct access to ` and 1-6
+     * - strong WASD/QERFG/ZXCVB coverage
+     * - no GUI on the base layer
+     * - no Delete or Backspace on the base layer
+     * - thumb cluster dedicated to layer access and core actions
+     * - entire physical right half left empty because it is not present
+     */
+    [_GAME] = LAYOUT_5row(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      _______, _______, _______, _______, _______, _______,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      _______, _______, _______, _______, _______, _______,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      _______, _______, _______, _______, _______, _______,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_6,    _______, _______, _______, _______, _______, _______, _______,
+        UTIL,     KC_ESC,    SWAP,     FUNC,    KC_LALT, KC_SPC, KC_ENT,  _______, _______, _______, _______, _______, _______, _______
     ),
-  /* SWAP
-   * ,-----------------------------------------.
-   * | Del  |   6  |   7  |   8  |   9  |   0  |
-   * |------+------+------+------+------+------|
-   * | Bksp |   Y  |   U  |   I  |   O  |   P  |
-   * |------+------+------+------+------+------|
-   * | Ctrl |   H  |   J  |   K  |   L  |   ;  |
-   * |------+------+------+------+------+------+------.
-   * | Shift|   N  |   M  |   ,  |   .  |   /  |  '   |
-   * |------+------+------+------+------+------+------|
-   * |      |      | Alt  | GUI  |      |      |  |   |
-   * `------------------------------------------------'
-   */
-  [_SWAP] = LAYOUT_5row( \
-      KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      _______, _______, _______, _______, _______, _______, \
-      KC_TAB,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                      _______, _______, _______, _______, _______, _______, \
-      KC_LCTL, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,                   _______, _______, _______, _______, _______, _______, \
-      KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC, _______, _______, _______, _______, _______, _______, _______, \
-      _______, KC_ESC, KC_LALT, KC_SPC, _______, _______, KC_ENT, _______, _______, _______, _______, _______, _______, _______ \
-      ),
+     
+    /* UTIL
+     * 6-0, arrow keys and print screen
+     * Plus WASD to keep moving around in games without switching layers
+     * Plus Modifiers to use with any of the above keys
+     */
+    [_UTIL] = LAYOUT_5row(
+        KC_TILD, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                       _______, _______, _______, _______, _______, _______,
+        KC_TAB,  KC_Q,     KC_W,   KC_E, KC_R, KC_PGUP,                           _______, _______, _______, _______, _______, _______,
+        KC_LCTL, KC_A,    KC_S,    KC_D, KC_UP,  KC_PGDN,                             _______, _______, _______, _______, _______, _______,
+        KC_LSFT, KC_Z,    KC_X,    KC_LEFT, KC_DOWN,    KC_RGHT, KC_PSCR, _______, _______, _______, _______, _______, _______, _______,
+        _______,     KC_ESC, SWAP,     FUNC,   KC_LALT, KC_SPC, KC_ENT,  _______, _______, _______, _______, _______, _______, _______
+    ),
 
-  /* FUNCTION
-   * ,-----------------------------------------.
-   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |
-   * |------+------+------+------+------+------|
-   * |      |   Q  |  Up  |   E  | Home | PGUP |
-   * |------+------+------+------+------+------|
-   * | Ctrl | Left | Down | Right|  End | PGDN |
-   * |------+------+------+------+------+------+------.
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |
-   * |------+------+------+------+------+------+------|
-   * |      |      | Alt  | GUI  |      |      |      |
-   * `------------------------------------------------'
-   */
-  [_FUNCTION] = LAYOUT_5row( \
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     _______, _______, _______, _______, _______, _______, \
-      KC_TAB, KC_Q,    KC_UP,   KC_E,    KC_HOME, KC_PGUP,                   _______, _______, _______, _______, _______, _______, \
-      KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGDN,                   _______, _______, _______, _______, _______, _______, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    _______, KC_DEL, _______, _______, _______, _______, _______, _______, _______, \
-      _______, KC_ESC, KC_LALT, KC_SPC, _______, _______, KC_ENT, _______, _______, _______, _______, _______, _______, _______ \
-      ),
+    /* FUNC/
+     * Function keys and home/end/pgup/pgdn for navigation
+     * Plus WASD to keep moving around in games without switching layers
+     * Plus Modifiers to use with any of the above keys
+     */
+    [_FUNC] = LAYOUT_5row(
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       _______, _______, _______, _______, _______, _______,
+        KC_TAB, KC_Q,     KC_W,   KC_E, KC_F9, KC_F10,                      _______, _______, _______, _______, _______, _______,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_UP,  KC_F11,                     _______, _______, _______, _______, _______, _______,
+        KC_LSFT,   KC_F7,   KC_F8,   KC_LEFT, KC_DOWN,    KC_RGHT,   KC_F12,  _______,  _______, _______, _______, _______, _______, _______,
+        UTIL,     KC_ESC,    SWAP,     _______,  KC_LALT, KC_SPC, KC_ENT,  _______,  _______, _______, _______, _______, _______, _______
+    ),
 
-  /* SWAPFX (SWAP + FUNCTION)
-   * ,-----------------------------------------.
-   * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
-   * |------+------+------+------+------+------|
-   * |      |   Q  |  Up  |   E  | Home | PGUP |
-   * |------+------+------+------+------+------|
-   * | Ctrl | Left | Down | Right|  End | PGDN |
-   * |------+------+------+------+------+------+------.
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |
-   * |------+------+------+------+------+------+------|
-   * |      |      | Alt  | GUI  |      |      |      |
-   * `------------------------------------------------'
-   */
-  [_SWAPFX] =  LAYOUT_5row( \
-      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    _______, _______, _______, _______, _______, _______, \
-      KC_TAB, KC_Q,    KC_UP,   KC_E,    KC_HOME, KC_PGUP,                   _______, _______, _______, _______, _______, _______, \
-      KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGDN,                   _______, _______, _______, _______, _______, _______, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_LGUI, KC_DEL, _______, _______, _______, _______, _______, _______, _______, \
-      _______, KC_ESC, KC_LALT, KC_SPC, _______, _______, KC_ENT, _______, _______, _______, _______, _______, _______, _______ \
-      )
-
+    /* SWAP
+     * Right half of keyboard with numbers and punctuation, for games that use those keys more than the left half
+     * Plus arrow keys
+     * Plus Modifiers to use with any of the above keys
+     */
+    [_SWAP] = LAYOUT_5row(
+        KC_TILD, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                     _______, _______, _______, _______, _______, _______,
+        KC_TAB, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                      _______, _______, _______, _______, _______, _______,
+        KC_LCTL, KC_H,    KC_J,    KC_K,    KC_UP,    KC_L,                   _______, _______, _______, _______, _______, _______,
+        KC_LSFT, KC_N,    KC_M,    KC_LEFT, KC_DOWN,    KC_RGHT,    KC_COMM, _______, _______, _______, _______, _______, _______, _______,
+        UTIL,     KC_ESC,    _______,     FUNC,  KC_LALT, KC_SPC, KC_ENT,  _______, _______, _______, _______, _______, _______, _______
+    ),
 };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SWAP, _FUNCTION, _SWAPFX);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case SWAPFX:
-      if (record->event.pressed) {
-        layer_on(_SWAP);
-        layer_on(_FUNCTION);
-      } else {
-        layer_off(_SWAP);
-        layer_off(_FUNCTION);
-      }
-      break;
-  }
-  return true;
-}
